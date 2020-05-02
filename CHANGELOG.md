7be87c56668afd849f1614ec2791fa0cf235265a
blacklizard <jeevanlx@gmail.com>
2020-05-02 16:44:14 +0800

    Add static mode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ All notable changes to this project will be documented in this file.
 ### Changed
 - Removed HTTP and replaced with TCP socket
 - LED data size reduced from 7 byte to 3 byte for each LED
+- Add static mode
 
 ## [0.0.2]
 - Pre release
--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,60 @@
 
 #include <FastLED.h>
 #include "WiFi.h"
- 
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASSWORD;
- 
+
+#define SCREEN 1
+#define STATIC 2
+
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
+
 WiFiServer server(8080);
-CRGB leds[NUM_LEDS]; 
- 
-void setup() {
+CRGB leds[NUM_LEDS];
+
+uint8_t mode[1];
+uint8_t color[3];
+void setup()
+{
   Serial.begin(115200);
   delay(1000);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
- 
+
   Serial.println("Connected to the WiFi network");
   Serial.print("Device IP: ");
   Serial.println(WiFi.localIP());
- 
+
   server.begin();
 
-  FastLED.addLeds<LED_CHIP, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_CHIP, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(UncorrectedColor);
   FastLED.setBrightness(BRIGHTNESS);
 }
- 
-void loop() {
+
+void loop()
+{
   WiFiClient client = server.available();
   while (client.connected() && client.available())
   {
-    client.read((uint8_t*)leds, NUM_LEDS*3);
-    delay(1);// needed to avoid random flickering of the LED strip
+    client.read(&mode[0], 1);
+    if (mode[0] == SCREEN)
+    {
+      client.read((uint8_t *)leds, NUM_LEDS * 3);
+    }
+    else if (mode[0] == STATIC)
+    {
+      client.read((uint8_t *)color, 3);
+      for (size_t i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i].red = color[0];
+        leds[i].green = color[1];
+        leds[i].blue = color[2];
+      }
+    }
+    delay(1); // needed to avoid random flickering of the LED strip
     FastLED.show();
   }
 }
